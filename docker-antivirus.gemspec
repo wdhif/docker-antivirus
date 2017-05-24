2c17227adf10c24007eddf9d0dbe87830c5f24a0
Wassim DHIF <wassimdhif@gmail.com>
2017-05-24 14:09:08 +0200

    bin added, helpers added, clamAV scan added, cleanup added

--- docker-antivirus.gemspec
+++ docker-antivirus.gemspec
@@ -14,9 +14,8 @@ Gem::Specification.new do |spec|
   spec.description   = 'Check your Docker Images for viruses with ClamAV'
   spec.homepage      = 'https://github.com/wdhif/docker-antivirus'
 
-  spec.files         = `git ls-files -z`.split("\x0")
-  spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
-  spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
+  spec.files         = []
+  spec.executables   = 'docker-antivirus'
   spec.require_paths = ['lib']
 
   spec.add_development_dependency 'thor'
--- lib/docker/antivirus/cli.rb
+++ lib/docker/antivirus/cli.rb
@@ -1,33 +1,35 @@
+require 'thor'
+
+require 'docker/antivirus/helpers'
+
 module Docker
   module Antivirus
     # Cli Class, used to define user interactions
     class Cli < Thor
       desc 'scan', 'Scan a docker image'
 
-      method_option :image, required: true, aliases: '-i'
+      method_option :image, required: false, aliases: '-i'
 
-      def call
-        directory = random_folder_name
-        puts directory
-        create_random_directory(directory)
-        sleep 1
-        cleanup(directory)
+      def scan
+        directory = Docker::Antivirus::Helpers.random_folder_name
+        Docker::Antivirus::Helpers.create_random_directory(directory)
+        Docker::Antivirus::Helpers.atomic_mount(options[:image], directory)
+        exit_status = Docker::Antivirus::Helpers.clamav_scan(options[:image], directory)
+        Docker::Antivirus::Helpers.cleanup(directory)
+        if exit_status == 0
+          puts "\e[32mNo virus detected\e[0m"
+          exit 0
+        else
+          puts "\e[31m!!! Virus detected !!!\e[0m"
+          exit 1
+        end
       end
 
-      def random_folder_name
-        ('a'..'z').to_a.sample(12).join
-      end
+      desc 'cleanup', 'Cleanup all folders'
 
-      def create_random_directory(directory)
-        `mkdir -p /docker-antivirus/#{directory}`
-      end
-
-      def cleanup(directory)
-        `rm -rf /docker-antivirus/#{directory}`
-      end
-
-      def atomic_mount
-        `atomic mount #{image}`
+      def cleanup
+        `rm -rf /docker-antivirus/*`
+        puts "All folders cleaned up"
       end
     end
   end
